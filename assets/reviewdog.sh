aa70c9d7070c52f79d118e98a1e6117f0f16d0a9
Andrea Brancaleoni <abc@pompel.me>
2023-01-19 00:54:36 +0100

    action: exit eagerly on error

--- assets/reviewdog.sh
+++ assets/reviewdog.sh
@@ -1,8 +1,8 @@
-#!/bin/bash -x
+#!/bin/bash -xe
 # Absolute path to this script. /home/user/bin/foo.sh
 SCRIPT=$(readlink -f $0)
 # Absolute path this script is in. /home/user/bin
 export SCRIPTPATH=`dirname $SCRIPT`
-reviewdog -reporter=github-pr-review -runners=semgrep,safesvg -conf="$SCRIPTPATH/reviewdog/reviewdog.yml" -tee
+reviewdog -reporter=github-pr-review -runners=semgrep,safesvg -conf="$SCRIPTPATH/reviewdog/reviewdog.yml" -fail-on-error -tee
 cat /dev/null semgrep.log safesvg.log > reviewdog.log
 find reviewdog.log -type f -empty -delete
