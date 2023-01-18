446c52028c8d2ebcbdf32c33057686c0b59a8063
Andrea Brancaleoni <abc@pompel.me>
2023-01-19 00:05:11 +0100

    xmllint: debug to stderr too

--- assets/xmllint.sh
+++ assets/xmllint.sh
@@ -3,4 +3,4 @@
 SCRIPT=$(readlink -f $0)
 # Absolute path this script is in. /home/user/bin
 export SCRIPTPATH=`dirname $SCRIPT`
-xmllint --dtdvalid $SCRIPTPATH/dtd/svg11-secure-flat.dtd --noout $1 2>&1 | grep -v '^Document'
+xmllint --dtdvalid $SCRIPTPATH/dtd/svg11-secure-flat.dtd --noout $1 2>&1 | tee /dev/stderr | grep -v '^Document'
