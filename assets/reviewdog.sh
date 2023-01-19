86eefd2f73461e122354c697c0ec82581c33a6b8
Andrea Brancaleoni <abc@pompel.me>
2023-01-19 16:39:15 +0100

    reviewdog: include blocklist

--- assets/reviewdog.sh
+++ assets/reviewdog.sh
@@ -3,6 +3,6 @@
 SCRIPT=$(readlink -f $0)
 # Absolute path this script is in. /home/user/bin
 export SCRIPTPATH=`dirname $SCRIPT`
-reviewdog -reporter=github-pr-review -runners=semgrep,safesvg -conf="$SCRIPTPATH/reviewdog/reviewdog.yml" -fail-on-error -tee
+reviewdog -reporter=github-pr-review -runners=semgrep,safesvg -conf="$SCRIPTPATH/reviewdog/reviewdog.yml" -tee
 cat /dev/null semgrep.log safesvg.log > reviewdog.log
 find reviewdog.log -type f -empty -delete
