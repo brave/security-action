341ed9889180054e49ceb9e013b64c9d00456571
Andrea Brancaleoni <abc@pompel.me>
2023-01-19 02:47:34 +0100

    action: fix utf8 names

--- assets/xmllint.sh
+++ assets/xmllint.sh
@@ -3,4 +3,8 @@
 SCRIPT=$(readlink -f $0)
 # Absolute path this script is in. /home/user/bin
 export SCRIPTPATH=`dirname $SCRIPT`
-xmllint --dtdvalid $SCRIPTPATH/dtd/svg11-secure-flat.dtd --noout $1 2>&1 | tee /dev/stderr | grep -v '^Document'
+
+# Only check SVGs
+[[ "$1" == *".svg" ]] || exit 0
+
+xmllint --dtdvalid $SCRIPTPATH/dtd/svg11-secure-flat.dtd --noout "$(printf "$1")" 2>&1 | tee /dev/stderr | grep -v '^Document'
