fe0e3bad861f3d346ea748e69883a3d629f90ef7
Andrea Brancaleoni <abc@pompel.me>
2023-02-08 00:55:34 +0100

    test

---
+++ t3sts2/tf/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = "~> 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.53.0"
+    }
+
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.42.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+provider "azurerm" {
+  features {}
+}
