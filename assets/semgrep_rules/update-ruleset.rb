7133cf656a1024c26680bdfbef99c6d4646fc1be
Andrea Brancaleoni <abc@pompel.me>
2023-02-08 00:37:26 +0100

    semgrep: clarify license

--- assets/semgrep_rules/update-ruleset.rb
+++ assets/semgrep_rules/update-ruleset.rb
@@ -27,48 +27,49 @@ RULESETS = [
 	'python',
 	'react',
 	'ruby',
-=begin
-	'bandit',
-	'brakeman',
+        'nextjs',
+        'nodejs',
+	'kotlin',
+	'supply-chain',
+	'react-team-tier',
+	'php-laravel',
+	'insecure-transport-jsnode',
+	'headless-browser',
+	'terraform',
+	'docker-compose',
+	'dockerfile',
+	'kubernetes',
 	'eslint',
 	'findsecbugs',
+	'brakeman',
+        'bandit',
 	'flawfinder',
 	'gitleaks',
 	'gosec',
 	'phpcs-security-audit',
 	'security-code-scan',
-
-	'docker-compose',
-	'dockerfile',
-	'kubernetes',
-	'nginx',
-	'terraform',
-
-	'nextjs',
-	'nodejs',
-	'kotlin',
 	'mobsfscan',
 	'wordpress',
-	'php-laravel',
+	'react-best-practices',
 	'trailofbits',
-	'supply-chain',
-	'react-team-tier',
-	'headless-browser',
-	'insecure-transport-jsnode',
-	'react-best-practices'
-=end
 ]
 
 HOST = 'https://semgrep.dev'
 GENERATED_DIR = "#{__dir__}/generated/"
+NONFREE_LICENSES = Set.new ['CC-BY-NC-SA-4.0', 'Commons Clause License Condition v1.0[LGPL-2.1-only]']
 
 FileUtils.mkdir_p GENERATED_DIR
 
 rules = {}
+nonfree_rules = {}
 vuln_rules = Set.new
+nonfree_vuln_rules = Set.new
 audit_rules = Set.new
+nonfree_audit_rules = Set.new
 security_noaudit_novuln_rules = Set.new
+nonfree_security_noaudit_novuln_rules = Set.new
 others_rules = Set.new
+nonfree_others_rules = Set.new
 
 BLOCKLIST = Set.new File.readlines("#{__dir__}/blocklist.txt").map(&:strip)
 
@@ -80,7 +81,11 @@ RULESETS.each do |ruleset|
 		"Connection" => "keep-alive").read)['rules']
 
 	ret.each do |rule|
-		rules[rule['id']] = rule unless BLOCKLIST.include? rule['metadata']['source']
+		if NONFREE_LICENSES.include? rule['metadata']['license']
+			nonfree_rules[rule['id']] = rule unless BLOCKLIST.include?(rule['metadata']['source'])
+		else 
+			rules[rule['id']] = rule unless BLOCKLIST.include?(rule['metadata']['source'])
+		end
 	end
 end
 
@@ -96,15 +101,41 @@ rules.each do |k, v|
 	end
 end
 
-puts "vulns.yaml containing #{vuln_rules.length} rules"
-puts "audit.yaml containing #{audit_rules.length} rules"
-puts "others.yaml containing #{others_rules.length} rules"
-puts "security_noaudit_novuln.yaml containing #{security_noaudit_novuln_rules.length} rules"
+nonfree_rules.each do |k, v|
+	if v['metadata']['category'] == 'security' && v['metadata']['subcategory'] != ['vuln'] && v['metadata']['subcategory'] != ['audit']
+		nonfree_security_noaudit_novuln_rules << v
+	elsif v['metadata']['category'] == 'security' && v['metadata']['subcategory'] == ['vuln']
+		nonfree_vuln_rules << v
+	elsif v['metadata']['category'] == 'security' && v['metadata']['subcategory'] == ['audit']
+		nonfree_audit_rules << v
+	else
+		nonfree_others_rules << v
+	end
+end
 
-File.write("#{GENERATED_DIR}/vulns.yaml", YAML.dump({"rules" => vuln_rules.to_a.sort {|a,b| a['id'] <=> b['id']}}))
-File.write("#{GENERATED_DIR}/security_noaudit_novuln.yaml", YAML.dump({"rules" => security_noaudit_novuln_rules.to_a.sort {|a,b| a['id'] <=> b['id']}}))
-File.write("#{GENERATED_DIR}/audit.yaml", YAML.dump({"rules" => audit_rules.to_a.sort {|a,b| a['id'] <=> b['id']}}))
-File.write("#{GENERATED_DIR}/others.yaml", YAML.dump({"rules" => others_rules.to_a.sort {|a,b| a['id'] <=> b['id']}}))
+puts "oss/vulns.yaml containing #{vuln_rules.length} rules"
+puts "oss/audit.yaml containing #{audit_rules.length} rules"
+puts "oss/others.yaml containing #{others_rules.length} rules"
+puts "oss/security_noaudit_novuln.yaml containing #{security_noaudit_novuln_rules.length} rules"
+
+FileUtils.mkdir_p("#{GENERATED_DIR}/oss")
+
+File.write("#{GENERATED_DIR}/oss/vulns.yaml", YAML.dump({"rules" => vuln_rules.to_a.sort {|a,b| a['id'] <=> b['id']}}))
+File.write("#{GENERATED_DIR}/oss/security_noaudit_novuln.yaml", YAML.dump({"rules" => security_noaudit_novuln_rules.to_a.sort {|a,b| a['id'] <=> b['id']}}))
+File.write("#{GENERATED_DIR}/oss/audit.yaml", YAML.dump({"rules" => audit_rules.to_a.sort {|a,b| a['id'] <=> b['id']}}))
+File.write("#{GENERATED_DIR}/oss/others.yaml", YAML.dump({"rules" => others_rules.to_a.sort {|a,b| a['id'] <=> b['id']}}))
+
+puts "nonfree/vulns.yaml containing #{nonfree_vuln_rules.length} rules"
+puts "nonfree/audit.yaml containing #{nonfree_audit_rules.length} rules"
+puts "nonfree/others.yaml containing #{nonfree_others_rules.length} rules"
+puts "nonfree/security_noaudit_novuln.yaml containing #{nonfree_security_noaudit_novuln_rules.length} rules"
+
+FileUtils.mkdir_p("#{GENERATED_DIR}/nonfree")
+
+File.write("#{GENERATED_DIR}/nonfree/vulns.yaml", YAML.dump({"rules" => nonfree_vuln_rules.to_a.sort {|a,b| a['id'] <=> b['id']}}))
+File.write("#{GENERATED_DIR}/nonfree/security_noaudit_novuln.yaml", YAML.dump({"rules" => nonfree_security_noaudit_novuln_rules.to_a.sort {|a,b| a['id'] <=> b['id']}}))
+File.write("#{GENERATED_DIR}/nonfree/audit.yaml", YAML.dump({"rules" => nonfree_audit_rules.to_a.sort {|a,b| a['id'] <=> b['id']}}))
+File.write("#{GENERATED_DIR}/nonfree/others.yaml", YAML.dump({"rules" => nonfree_others_rules.to_a.sort {|a,b| a['id'] <=> b['id']}}))
 
 # require 'pry'
 # binding.pry
