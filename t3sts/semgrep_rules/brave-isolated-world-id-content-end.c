0688898a3cb552d117c4d026442ff89ad57530f7
bcaller <bcaller@brave.com>
2023-02-18 21:31:18 +0000

    Fix C self-tests

--- t3sts/semgrep_rules/brave-isolated-world-id-content-end.c
+++ t3sts/semgrep_rules/brave-isolated-world-id-content-end.c
@@ -1,6 +1,8 @@
 constexpr int kBraveAdsIsolatedWorldId =
+    // ruleid: brave-isolated-world-id-content-end
     content::ISOLATED_WORLD_ID_CONTENT_END + 4;
 
 int main() {
+    // ruleid: brave-isolated-world-id-content-end
    int a = content::ISOLATED_WORLD_ID_CONTENT_END;
 }
