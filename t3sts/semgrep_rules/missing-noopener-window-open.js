b2abc501e0816a42923b96de10d791c0334538a1
Andrea Brancaleoni <abc@pompel.me>
2023-02-23 11:40:55 +0100

    semgrep_rules missing-noopener-window-open: less noise

--- t3sts/semgrep_rules/missing-noopener-window-open.js
+++ t3sts/semgrep_rules/missing-noopener-window-open.js
@@ -1,9 +1,9 @@
-// ruleid: missing-noopener-window-open
+// ruleid: missing-noopener-window-open-native
 window.open("something")
-// ruleid: missing-noopener-window-open
+// ruleid: missing-noopener-window-open-native
 window.open("ciao", "biao")
-// ruleid: missing-noopener-window-open
+// ruleid: missing-noopener-window-open-native
 open("ciao", "ciao")
 
-// ok: missing-noopener-window-open
+// ok: missing-noopener-window-open-native
 window.open("ciao", "bao", "noopener", "asd")
