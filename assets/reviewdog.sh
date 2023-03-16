f74f4d1dbc01a91396782852bc05031132d63b07
bcaller <bcaller@brave.com>
2023-03-16 11:09:44 +0800

    Sometimes reporting to Github will fail. Print log message.

--- assets/reviewdog.sh
+++ assets/reviewdog.sh
@@ -14,9 +14,18 @@ if [ -n "${GITHUB_BASE_REF+set}" ]; then
     done
 
     for runner in $RUNNERS; do
-        cat $runner.log | reviewdog -reporter=github-pr-review -efm='%f:%l: %m'
+        cat $runner.log | reviewdog -reporter=github-pr-review -efm='%f:%l: %m' \
+          || cat $runner.log >> reviewdog.fail.log
         cat $runner.log >> reviewdog.log
     done
+
+    if [[ -f "reviewdog.fail.log" ]]; then
+        set +x
+        echo -e '\033[0;31mThis action encountered an error while reporting the following findings via the Github API:'
+        cat reviewdog.fail.log | sed 's/^/\x1B[0;34m/'
+        echo -e '\033[0;31mThe failure of this action should not prevent you from merging your PR. Please report this failure to the maintainers of https://github.com/brave/security-action \033[0m'
+        exit 1
+    fi
 else
     find $SCRIPTPATH/../t3sts/ | sed "s|$SCRIPTPATH/../||g" | tr '\n' '\0' > $SCRIPTPATH/all_changed_files.txt
     GITHUB_BASE_REF=initial-commit reviewdog  -runners=semgrep,safesvg -conf="$SCRIPTPATH/reviewdog/reviewdog.yml"  -diff="git diff origin/$GITHUB_BASE_REF" -reporter=local -tee
