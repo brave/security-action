10a4c9d9173f3b83033fb94c47c9b89a8f9a1fdb
bcaller <bcaller@brave.com>
2023-03-20 12:25:56 +0800

    Make interesting API calls rule unmaintainable
    We are looking specifically for function calls.
    
    So put some sem in the grep.
    
    But now we are left with a horrific regex.
    
    I could optimise it but that would leave it much worse

---
+++ t3sts/semgrep_rules/interesting-api-calls.cc
@@ -0,0 +1,10 @@
+// ruleid:raptor-interesting-api-calls
+amazing_scanf();
+
+// ruleid:raptor-interesting-api-calls
+memset(str, 'x', 10);
+
+// ok:raptor-interesting-api-calls
+void MyService::MaybeReconnect(TaskList tasks, int reconnect) {
+    return;
+}
