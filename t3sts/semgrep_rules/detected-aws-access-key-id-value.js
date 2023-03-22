af2db0a6dccab47bad4f190ce2cad0dd169b5c37
bcaller <bcaller@brave.com>
2023-03-22 13:06:30 +0800

    Don't match AWS regexes in the middle of words

---
+++ t3sts/semgrep_rules/detected-aws-access-key-id-value.js
@@ -0,0 +1,10 @@
+// ruleid: detected-aws-access-key-id-value
+var AWS_ACCESS_KEY_ID = "AKIABBBBBBBBBBBBBB1B";
+
+// ruleid: detected-aws-access-key-id-value
+/*
+AKIABBBBBBBBBBBBBB1B
+*/
+
+// ok: detected-aws-access-key-id-value
+const SOUVLAKIAISTOTALLYDELICIOUS = true;
