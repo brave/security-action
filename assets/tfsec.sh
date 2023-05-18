8f045afb2da0a8be31a44f5ee4bde71e3cf9e33f
Andrea Brancaleoni <miwaxe@gmail.com>
2023-05-18 13:15:25 +0200

    tfsec: don't fail eagerly

--- assets/tfsec.sh
+++ assets/tfsec.sh
@@ -4,4 +4,4 @@ SCRIPT=$(readlink -f $0)
 # Absolute path this script is in. /home/user/bin
 export SCRIPTPATH=`dirname $SCRIPT`
 
-tfsec "$1" --format=json | jq -r -f "$SCRIPTPATH/tfsec.jq"
+(tfsec "$1" --format=json | jq -r -f "$SCRIPTPATH/tfsec.jq" ) || exit 0
