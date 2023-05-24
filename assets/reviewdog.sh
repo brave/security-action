6e0d2b42b6de7d579a8562e726158689ece92201
Andrea Brancaleoni <abc@pompel.me>
2023-05-24 18:32:31 +0200

    action.yml: slack failure

--- assets/reviewdog.sh
+++ assets/reviewdog.sh
@@ -19,14 +19,6 @@ if [ -n "${GITHUB_BASE_REF+set}" ]; then
         cat $runner.log >> reviewdog.log
         wc -l $runner.log
     done
-
-    if [[ -f "reviewdog.fail.log" ]]; then
-        set +x
-        echo -e '\033[0;31mThis action encountered an error while reporting the following findings via the Github API:'
-        cat reviewdog.fail.log | sed 's/^/\x1B[0;34m/'
-        echo -e '\033[0;31mThe failure of this action should not prevent you from merging your PR. Please report this failure to the maintainers of https://github.com/brave/security-action \033[0m'
-        exit 1
-    fi
 else
     find $SCRIPTPATH/../t3sts/ | sed "s|$SCRIPTPATH/../||g" | tr '\n' '\0' > $SCRIPTPATH/all_changed_files.txt
     GITHUB_BASE_REF=initial-commit reviewdog  -runners=semgrep,safesvg,sveltegrep,pip-audit,npm-audit -conf="$SCRIPTPATH/reviewdog/reviewdog.yml"  -diff="git diff origin/$GITHUB_BASE_REF" -reporter=local -tee
