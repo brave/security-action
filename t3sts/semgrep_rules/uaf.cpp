7b846d1cfbc8184b855f58558150fa6e41b0d2fa
Andrea Brancaleoni <abc@pompel.me>
2023-06-14 19:45:13 +0200

    t3sts/semgrep_rules/uaf.cpp: fix

--- t3sts/semgrep_rules/uaf.cpp
+++ t3sts/semgrep_rules/uaf.cpp
@@ -13,13 +13,14 @@ v8::Local<v8::Promise> uaf(v8::Isolate* isolate) {
       v8::Global<v8::Context>(isolate, isolate->GetCurrentContext()));
   auto promise_resolver(
       v8::Global<v8::Promise::Resolver>(isolate, resolver.ToLocalChecked()));
-  auto context(v8::Global<v8::Context>(isolate, isolate->GetCurrentContext()));
-  // ruleid: chromium-bind-uaf
-  provider->IsLocked(&OnIsUnlocked, weak_ptr_factory_.GetWeakPtr(),
-      std::move(global_context), std::move(promise_resolver), isolate));
-  // ok: chromium-bind-uaf
-  provider->IsLocked(base::Unretained(this), weak_ptr_factory_.GetWeakPtr(),
-      std::move(global_context), std::move(promise_resolver), isolate));
-
-  return resolver.ToLocalChecked()->GetPromise();
+	// chromium-bind-uaf
+  DistillPageText(
+      render_frame(),
+      base::BindOnce(&PageContentExtractor::OnDistillResult,
+                     weak_ptr_factory_.GetWeakPtr(), std::move(callback)));
+	// ok: chromium-bind-uaf
+  DistillPageText(
+      render_frame(),
+      base::BindOnce(&PageContentExtractor::OnDistillResult,
+                     base::Unretained(this), std::move(callback)));
 }
