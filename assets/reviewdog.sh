4aee0322e47a7dcab8bbaa0bed38cdef97dc4c80
Andrea Brancaleoni <abc@pompel.me>
2023-06-21 19:52:48 +0200

    reviewdog.sh: remove empty file

--- assets/reviewdog.sh
+++ assets/reviewdog.sh
@@ -45,3 +45,4 @@ cat reviewdog.log | grep 'failed with zero findings: The command itself failed' 
 echo "findings=$(cat reviewdog.log | grep '^[A-Z]:[^:]*:' | wc -l)" >> $GITHUB_OUTPUT
 
 find reviewdog.log -type f -empty -delete
+find reviewdog.fail.log -type f -empty -delete
