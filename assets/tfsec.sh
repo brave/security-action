c2e91fa2c9ff590f07e7085bab698b2216235586
Andrea Brancaleoni <abc@pompel.me>
2023-06-28 15:39:15 +0200

    tfsec.sh: use tfvars if present

--- assets/tfsec.sh
+++ assets/tfsec.sh
@@ -4,4 +4,11 @@ SCRIPT=$(readlink -f $0)
 # Absolute path this script is in. /home/user/bin
 export SCRIPTPATH=`dirname $SCRIPT`
 
-tfsec "$1" --format=json | jq -r -f "$SCRIPTPATH/tfsec.jq"
+ARGS=""
+if ls *.tfvars 2> /dev/null; then
+    for TFVARS in *.tfvars; do
+        ARGS+="--tfvars-file $TFVARS "
+    done
+fi
+
+tfsec "$1" $ARGS --format=json | jq -r -f "$SCRIPTPATH/tfsec.jq"
