a36f82c42e3b872e38a85d710fc212b3a1b72ac3
Andrea Brancaleoni <miwaxe@gmail.com>
2023-08-04 14:13:22 +0200

    nodejs-insecure-url-parse.js test: add more t cases

--- t3sts/semgrep_rules/nodejs-insecure-url-parse.js
+++ t3sts/semgrep_rules/nodejs-insecure-url-parse.js
@@ -1,2 +1,17 @@
 // ruleid: nodejs-insecure-url-parse
 url.parse("here lies dragons");
+// ruleid: nodejs-insecure-url-parse
+require('url').parse("here lies dragons");
+
+var uparser = require('url');
+
+// ruleid: nodejs-insecure-url-parse
+uparser.parse("here lies dragons");
+
+function() {
+  // ruleid: nodejs-insecure-url-parse
+  uparser.parse("here lies dragons");
+}
+
+// ruleid: nodejs-insecure-url-parse
+setTimeout(()=> uparser.parse("here lies dragons"), 1000);
