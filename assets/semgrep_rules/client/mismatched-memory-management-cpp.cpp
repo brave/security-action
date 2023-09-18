05703fe0d025f266b7fbbebfa6dc8d3d4b864f0b
bcaller <bcaller@brave.com>
2023-09-18 19:19:12 +0100

    fix mismatched-memory-management-cpp
    C++ was matching 'delete;'.
    
    This is for removing the default implementation inherited from a base class
    rather than a memory error.
    
    Prevent this particular false positive.

--- assets/semgrep_rules/client/mismatched-memory-management-cpp.cpp
+++ assets/semgrep_rules/client/mismatched-memory-management-cpp.cpp
@@ -1,4 +1,73 @@
+// ok: raptor-mismatched-memory-management-cpp
+Blah& operator=(const Blah&) = delete;
+
 void RedeemOptedOutConfirmation::Destroy() {
   // ok: raptor-mismatched-memory-management-cpp
   delete this;
+}
+
+void bad1()
+{
+	BarObj *ptr = new BarObj()
+
+	// ruleid: raptor-mismatched-memory-management-cpp
+	free(ptr);
+}
+
+void good1()
+{
+	BarObj *ptr = new BarObj()
+
+	// ok: raptor-mismatched-memory-management-cpp
+	delete ptr;
+}
+
+class A {
+	void bad2();
+	void good2();
+};
+
+void A::bad2()
+{
+	int *ptr;
+	ptr = (int*)malloc(sizeof(int));
+
+	// ruleid: raptor-mismatched-memory-management-cpp
+	delete ptr;
+}
+
+void A::good2()
+{
+	int *ptr;
+	ptr = (int*)malloc(sizeof(int));
+
+	// ok: raptor-mismatched-memory-management-cpp
+	free(ptr);
+}
+
+class B {
+	void bad3(bool);
+	void good3();
+};
+
+void B::bad3(bool heap) {
+	int localArray[2] = { 11,22 };
+	int *p = localArray;
+
+	if (heap) {
+		p = new int[2];
+	}
+
+	// ruleid: raptor-mismatched-memory-management-cpp
+	delete[] p;
+}
+
+void B::good3() {
+	int localArray[2] = { 11,22 };
+	int *p = localArray;
+
+	p = new (std::nothrow) int[2];
+
+	// ok: raptor-mismatched-memory-management-cpp
+	delete[] p;
 }
