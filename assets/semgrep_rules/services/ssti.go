025f5f838e94b956ae778835adb923743bed543d
Andrea Brancaleoni <abc@pompel.me>
2023-10-11 15:49:21 +0200

    *: readd missing golang rules

---
+++ assets/semgrep_rules/services/ssti.go
@@ -0,0 +1,89 @@
+// LICENSE: Commons Clause License Condition v1.0[LGPL-2.1-only]
+// original source: https://github.com/returntocorp/semgrep-rules/blob/5b098c252feec688d243cef046d07597a546c25b/go/template/security/insecure-types.go
+
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+type User struct {
+	ID       int
+	Email    string
+	Password string
+}
+
+func match1(w http.ResponseWriter, req *http.Request) {
+
+	var user1 = &User{1, "user@gmail.com", "Sup3rSecr3t123!"}
+	query := req.URL.Query().Get("query")
+	// ruleid:go-ssti
+	var text = fmt.Sprintf(`
+	<html>
+	<head>
+	<title>SSTI</title>
+	</head>
+	<body>
+		<h2>Hello {{ .Email }}</h2>
+		<p>Search result for %s</p>
+	</body></html>
+	`, query)
+	tmpl := template.New("hello")
+	tmpl, err := tmpl.Parse(text)
+	if err != nil {
+		fmt.Println(err)
+	}
+	tmpl.Execute(w, user1)
+}
+
+func match2(w http.ResponseWriter, req *http.Request) {
+
+	var user1 = &User{1, "user@gmail.com", "Sup3rSecr3t123!"}
+	if err := req.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	query := req.Form.Get("query")
+	// ruleid:go-ssti
+	var text = fmt.Sprintf(`
+	<html>
+	<head>
+	<title>SSTI</title>
+	</head>
+	<body>
+		<h2>Hello {{ .Email }}</h2>
+		<p>Search result for %s</p>
+	</body></html>
+	`, query)
+	tmpl := template.New("hello")
+	tmpl, err := tmpl.Parse(text)
+	if err != nil {
+		fmt.Println(err)
+	}
+	tmpl.Execute(w, user1)
+}
+
+func no_match(w http.ResponseWriter, req *http.Request) {
+
+	var user1 = &User{1, "user@gmail.com", "Sup3rSecr3t123!"}
+	query := "constant string"
+	// ok:go-ssti
+	var text = fmt.Sprintf(`
+	<html>
+	<head>
+	<title>SSTI</title>
+	</head>
+	<body>
+		<h2>Hello {{ .Email }}</h2>
+		<p>Search result for %s</p>
+	</body></html>
+	`, query)
+	tmpl := template.New("hello")
+	tmpl, err := tmpl.Parse(text)
+	if err != nil {
+		fmt.Println(err)
+	}
+	tmpl.Execute(w, user1)
+}
