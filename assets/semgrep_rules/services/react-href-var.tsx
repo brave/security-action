d9d895969815a2d90a013121c111c80576896306
bcaller <bcaller@brave.com>
2023-10-31 12:00:41 +0000

    Exempt SecureLink from scheme checks #414
    Forks rule

---
+++ assets/semgrep_rules/services/react-href-var.tsx
@@ -0,0 +1,69 @@
+// ok: react-href-var
+let zzz = <Foo className={"foobar"} href={getQuery()} />;
+
+function test1(input) {
+  // ruleid: react-href-var
+  const params = {href: input.a};
+  return React.createElement("a", params);
+}
+
+// ok: react-href-var
+{collaborationSectionData.paragraphs.map((item, i) => (
+  <div>  <a href={item.value}>click</a></div>
+))}
+
+// ok: react-href-var
+let zzz = <Foo className={"foobar"} href={`${input}`} />;
+
+// ok: react-href-var
+let zzz = <Foo className={"foobar"} href={SEMGREP_REPO} />;
+
+// ok: react-href-var
+let zzz = <Foo className={"foobar"} href={SEMGREP_REPO1} />;
+
+function test1(input) {
+// ok: react-href-var
+  if(input.startsWith("https:")) {
+    const params = {href: input};
+    return React.createElement("a", params);
+  }
+}
+
+function test2(input) {
+  // ok: react-href-var
+  const params = {href: "#"+input};
+  return React.createElement("a", params);
+}
+
+function test2(input) {
+  // ok: react-href-var
+  const params = {href: "#"+input};
+  return React.createElement("a", params);
+}
+
+
+// ok: react-href-var
+const b = <a className={"foobar"} href={"http://www.example.com"}></a>;
+
+// ok: react-href-var
+let x = <a className={"foobar"} href={"#"+input}></a>;
+
+// ok: react-href-var
+let x = <a className={"foobar"} href={`#${input}`}></a>;
+
+function okTest1() {
+// ok: react-href-var
+  return React.createElement("a", {href: "https://www.example.com"});
+}
+
+function F({ info }) {
+  const u = info.data.url.url;
+  // ok: react-href-var
+  return <SecureLink href={u}>Link</SecureLink>
+}
+
+function G({ info }) {
+    const u = info.data.url.url;
+    // ruleid: react-href-var
+    return <BadLink href={u}>Link</BadLink>
+  }
