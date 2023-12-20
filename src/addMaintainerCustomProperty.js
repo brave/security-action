4cbf2f07358318a5c2629354e4238ee5d9ec653b
Andrea Brancaleoni <ab.gh@pompel.me>
2023-12-20 06:19:04 +0100

    New workflows: add-maintainer-custom-property, dependabot-nudge

---
+++ src/addMaintainerCustomProperty.js
@@ -0,0 +1,168 @@
+export default async function addMaintainerCustomProperty({
+    org,
+    githubToken = null,
+    github = null,
+    ignoreMaintainers = [],
+    debug = false,
+    simpleScan = false,
+    skipRepositories = ["chromium"],
+}) {
+    const watermark = "The following repositories should be archived since they don't have any maintainer:\n";
+    let output = "";
+
+    if (!github && githubToken) {
+        const { Octokit } = await import("octokit");
+
+        github = new Octokit({ auth: githubToken })
+    }
+
+    if (!github && !githubToken) {
+        throw new Error('either githubToken or github is required!');
+    }
+
+    debug = debug === 'true' || debug === true;
+    simpleScan = simpleScan === 'true' || simpleScan === true;
+
+    if (debug)
+        console.log(`simpleScan: ${simpleScan} ${typeof simpleScan}`);
+
+    // if ignoreMaintainers is a string, split it on commas
+    if (typeof ignoreMaintainers === 'string') {
+        ignoreMaintainers = ignoreMaintainers.split(',');
+    }
+
+    // if skipRepositories is a string, split it on commas
+    if (typeof skipRepositories === 'string') {
+        skipRepositories = skipRepositories.split(',');
+    }
+
+    // get all repositories in this organization
+    const repos = Array.from(await github.paginate(github.rest.repos.listForOrg, {
+        org: org,
+        type: 'all',
+    })).filter(r => r.archived === false).filter(r => r.disabled === false);
+
+    if (debug)
+        console.log(`repos len: ${repos.length}`);
+
+    // get all the users in this organization
+    const orgUsers = await github.paginate(github.rest.orgs.listMembers, {
+        org: org,
+    });
+
+    if (debug)
+        console.log(`orgUsers len: ${orgUsers.length}, ${orgUsers.map(u => u.login)}`);
+
+    // list contributors for each repository
+    for (const repo of repos) {
+        if (simpleScan === false)
+            break;
+
+        if (skipRepositories.includes(repo.name))
+            continue;
+
+        try {
+
+            if (debug)
+                console.log(`simple scanning repo ${repo.name} in org ${org}`);
+    
+            const contributors = await github.paginate(github.rest.repos.listContributors, {
+                owner: org,
+                repo: repo.name,
+            });
+    
+            // filter contributors to only those who are members of the organization
+            const orgContributors = contributors.
+                filter(c => orgUsers.find(u => u.login === c.login)).
+                sort((a, b) => b.contributions - a.contributions).map(c => c.login);
+    
+            if (orgContributors.length > 0) {
+                await github.request('PATCH /orgs/{org}/properties/values', {
+                    org: org,
+                    repository_names: [repo.name],
+                    properties: [
+                        {
+                            property_name: 'maintainers',
+                            value: orgContributors.slice(0, 3).join(",") || null,
+                        }
+                    ],
+                    headers: {
+                        'X-GitHub-Api-Version': '2022-11-28'
+                    }
+                });
+            } else {
+                if (!(repo.private || repo.archived || repo.disabled)) {
+                    output += `- https://github.com/${org}/${repo.name} ${org}/${repo.name}\n`;
+                }
+            }    
+        } catch (e) {
+            if (debug) console.log(e);
+        }
+    }
+
+    // filter out archived repositories
+    // get all the commits for each repository, after a certain date
+    for (const repo of repos) {
+        if (simpleScan === true)
+            break;
+
+        if (skipRepositories.includes(repo.name))
+            continue;
+
+        if (debug)
+            console.log(`scanning repo ${repo.name} in org ${org}`);
+
+        let repoMaintainers = {};
+
+        const commits = await github.paginate(github.rest.repos.listCommits, {
+            owner: org,
+            repo: repo.name,
+            since: new Date(((d) => d.setDate(d.getDate() - 2 * 365))(new Date())).toISOString(), // older than 2 years
+        });
+
+        // get the author of each commit
+        for (const commit of commits) {
+            // create a map between the author and the number of commits
+            if (commit.author) {
+                if (repoMaintainers[commit.author.login]) {
+                    repoMaintainers[commit.author.login] += 1;
+                } else {
+                    repoMaintainers[commit.author.login] = 1;
+                }
+            }
+        }
+
+        // create an ordered array by number of commits
+        const sortedRepoMaintainers = Object.keys(repoMaintainers).
+            sort((a, b) => repoMaintainers[b] - repoMaintainers[a]).filter(c => orgUsers.find(u => u.login === c)).
+            filter(u => !u.endsWith("[bot]")).filter(u => !ignoreMaintainers.includes(u)).slice(0, 3);
+
+        if (debug) console.log(`sortedRepoMaintainers: ${sortedRepoMaintainers}`);
+
+        if (sortedRepoMaintainers.length > 0) {
+            await github.request('PATCH /orgs/{org}/properties/values', {
+                org: org,
+                repository_names: [repo.name],
+                properties: [
+                    {
+                        property_name: 'maintainers',
+                        value: sortedRepoMaintainers.join(",") || null,
+                    }
+                ],
+                headers: {
+                    'X-GitHub-Api-Version': '2022-11-28'
+                }
+            });
+        } else {
+            if (!(repo.private || repo.archived || repo.disabled)) {
+                output += `- https://github.com/${org}/${repo.name} ${org}/${repo.name}\n`;
+            }
+        }
+    }
+
+    if (output.length > 0) {
+        output = watermark + output;
+    }
+
+    return output;
+};
---
+++ src/dependabotNudge.js
@@ -0,0 +1,138 @@
+const Severity = {
+    low: 0,
+    medium: 1,
+    high: 2,
+    critical: 3,
+}
+
+export default async function dependabotNudge({
+    org,
+    githubToken = null,
+    github = null,
+    debug = false,
+    minlevel = Severity.high,
+    skipRepositories = ['chromium'],
+    skipHotwords = ['dos', 'denial of service', 'redos', 'denial-of-service', 'memory explosion'],
+    defaultContact = ['yan'],
+    singleOutputMessage = false,
+}) {
+    if (!github && githubToken) {
+        const { Octokit } = await import("octokit");
+
+        github = new Octokit({ auth: githubToken })
+    }
+
+    if (!github && !githubToken) {
+        throw new Error('either githubToken or github is required!');
+    }
+
+    const messages = [];
+
+    debug = debug === 'true' || debug === true;
+    singleOutputMessage = singleOutputMessage === 'true' || singleOutputMessage === true;
+
+    // if skipRepositories is a string, split it on commas
+    if (typeof skipRepositories === 'string') {
+        skipRepositories = skipRepositories.split(',');
+    }
+
+    // if skipHotwords is a string, split it on commas
+    if (typeof skipHotwords === 'string') {
+        skipHotwords = skipHotwords.split(',');
+    }
+
+    // if minlevel is a string, convert to Severity enum
+    if (typeof minlevel === 'string') {
+        minlevel = Severity[minlevel];
+    }
+    
+    // if defaultContact is a string, split it on commas
+    if (typeof defaultContact === 'string') {
+        defaultContact = defaultContact.split(',');
+    }
+
+    // get all repositories in this organization
+    const repos = Array.from(await github.paginate(github.rest.repos.listForOrg, {
+        org: org,
+        type: 'all',
+    })).filter(r => r.archived === false).filter(r => r.disabled === false);
+
+    const properties = await github.paginate('GET /orgs/{org}/properties/values', {
+        org: org,
+        headers: {
+            'X-GitHub-Api-Version': '2022-11-28'
+        },
+    });
+
+    // hash out of properties array
+    const props = {};
+    for (const prop of properties) {
+        props[prop.repository_name] = prop.properties.reduce((obj, item) => {
+            obj[item.property_name] = item.value;
+            return obj;
+        }, {});
+    }
+    
+    // get dependabot alerts for each repository
+    for (const repo of repos) {
+        if (debug)
+            console.log(`scanning repo ${repo.name} in org ${org}`);
+        
+        if (skipRepositories.includes(repo.name)) {
+            continue;
+        }
+
+        try {
+            const alerts = Array.from(await github.paginate('GET /repos/{owner}/{repo}/dependabot/alerts', {
+                owner: org,
+                repo: repo.name,
+                headers: {
+                  'X-GitHub-Api-Version': '2022-11-28'
+                },
+                sort: 'updated',
+                state: 'open',
+                severity: Object.keys(Severity).filter(s => Severity[s] >= minlevel)
+            })).filter(a => !skipHotwords.some(h => a.security_advisory.summary.toLowerCase().includes(h)));
+
+            // get property values for this repository
+            const prop = props[repo.name] || {properties: {}};
+            const maintainers = (prop.maintainers || "").split(',').filter(Boolean).map(m => `@${m}`) || [];
+            
+            if (alerts.length > 0) {
+                if (debug)
+                    console.log(`alerts len: ${alerts.length}`);
+
+                let msg = `[${org}/${repo.name}](https://github.com/${org}/${repo.name}) has \`${alerts.length}\` open security issue(s) in depedencies`;
+                const critLen = alerts.filter(s => Severity[s.severity] >= Severity.critical).filter.length;
+                if (critLen > 0) {
+                    msg += `, **\`${critLen}\` of which are critical**`;
+                }
+                msg += `\n\n---\n\n`;
+
+                for (const alert of alerts) {
+                    msg += `\`${alert.dependency.package.name}\` by \`${alert.security_advisory.cve_id || alert.security_advisory.ghsa_id}\` with a \`${alert.security_advisory.severity}\` severity *${alert.security_advisory.summary}*`;
+                    msg += `\n\n`; 
+                    msg += alert.security_advisory.description.split("\n").map(d => `&gt; ${d}`).join("\n");
+                    msg += `\n\n`;
+                    msg += `Handle this alert at ${alert.html_url}\n\n`
+                    msg += "\n\n---\n\n"
+                }
+
+                if (maintainers.length > 0) {
+                    msg += `Maintainers: ${maintainers.join(', ')}`;
+                } else {
+                    msg += `**No maintainers listed for the given vulnerabilities, consider migrating and archiving this repository** - ${defaultContact.map(c => `@${c}`).join(', ')}`;
+                }
+
+                messages.push(msg);
+            }
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    if (debug || singleOutputMessage)
+        return messages.join("\n\n");
+    else
+        return messages;
+}
