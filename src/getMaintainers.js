e06c36939ab3f23da11029001e0aba5cc03ab6ca
Andrea Brancaleoni <ab.gh@pompel.me>
2023-12-22 00:58:28 +0100

    New function: getMaintainers

---
+++ src/getMaintainers.js
@@ -0,0 +1,41 @@
+export default async function getMaintainers({
+    org,
+    githubToken = null,
+    github = null,
+}) {
+    if (!github && githubToken) {
+        const { Octokit } = await import("octokit");
+
+        github = new Octokit({ auth: githubToken })
+    }
+
+    if (!github && !githubToken) {
+        throw new Error('either githubToken or github is required!');
+    }
+
+    const properties = await github.paginate('GET /orgs/{org}/properties/values', {
+        org: org,
+        headers: {
+            'X-GitHub-Api-Version': '2022-11-28'
+        },
+    });
+
+    // hash out of properties array
+    const props = {};
+    for (const prop of properties) {
+        props[prop.repository_name] = prop.properties.reduce((obj, item) => {
+            obj[item.property_name] = item.value;
+            return obj;
+        }, {});
+    }
+
+    let maintainers = "";
+
+    for (const repo of Object.keys(props).sort()) {
+        if (props[repo].maintainers) {
+            maintainers += `- https://github/${org}/${repo} maintainers: ${props[repo].maintainers.split(',').join(', ')}\n`
+        }
+    }
+
+    return maintainers;
+}
