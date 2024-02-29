a2865c634d30127370520816fc65efa409e54d29
bcaller <bcaller@brave.com>
2024-02-29 11:51:47 +0800

    r.ParseMultipartForm and io.ReadAll update
    It's not super serious, but caught my eye.

--- assets/semgrep_rules/services/io-readall-dos.go
+++ assets/semgrep_rules/services/io-readall-dos.go
@@ -1,8 +1,19 @@
-// ruleid: io-readall-dos
-io.ReadAll(r.Body)
+func handleBad(w http.ResponseWriter, r *http.Request) []byte {
+	// ruleid: io-readall-dos
+	payload, _ = io.ReadAll(r.Body)
+	return payload
+}
+
+func handleOK(w http.ResponseWriter, r *http.Request) []byte {
+	r.Body = http.MaxBytesReader(w, r.Body, 123)
+	fmt.Print("banana")
+	// ok: io-readall-dos
+	payload, _ = io.ReadAll(r.Body)
+	return payload
+}
 
 // ok: io-readall-dos
-io.ReadAll(http.MaxBytesReader(w, r.Body, u.maxRequestSize))
+io.ReadAll(io.LimitReader(r.Body, u.maxRequestSize))
 
 // ok: io-readall-dos
 io.ReadAll(x.y)
