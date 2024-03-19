d14e3baefa2b05cd24b7524a5226af0e8096cf93
Andrea Brancaleoni <abc@pompel.me>
2024-03-19 14:46:08 -0300

    reviewdog: upgrade from grep based to fnmatch based, add per-repo setting

--- assets/cleaner.rb
+++ assets/cleaner.rb
@@ -1,20 +1,60 @@
 #!/usr/bin/env ruby
 require 'optparse'
 
-options = {}
+DEFAULT_MATCHER_FILENAME = ".github/security-action-blocklist.txt"
+
+class Matcher
+  def initialize(*blocklist_files)
+    @blocklist = []
+    blocklist_files.each do |blf|
+      next unless File.exist?(blf)
+
+      blocklist = File.read(blf).split("\n").map(&:strip).reject(&:empty?)
+      # remove empty lines and comments
+      blocklist.reject! { |r| r.empty? || r.start_with?('#') }
+
+      # remove all matching lines and report
+      blocklist.reject! do |r|
+        ret = r =~ /^[*@]+$/
+        STDERR.puts "Warning: #{blf} contains a line with only asterisks/at, which will match everything" if ret
+        ret
+      end        
+
+      @blocklist += blocklist
+    end
+  end
+
+  def match?(line)
+    @blocklist.each do |r|
+      return true if File.fnmatch?("*#{r}*", line)
+    end
+    false
+  end
+end
+
+options = {
+  matcher: Matcher.new(DEFAULT_MATCHER_FILENAME)
+}
 OptionParser.new do |opts|
   opts.banner = "Usage: reviewdog-adapter.rb [options]"
 
   opts.on("--svgo", "Add SVGO String") do |v|
     options[:svgo] = true
+    options[:matcher] = Matcher.new(DEFAULT_MATCHER_FILENAME, "#{ENV["SCRIPTPATH"]}/dtd/blocklist.txt")
   end
 
-  opts.on("--assignees", "Add SVGO String") do |v|
+  opts.on("--assignees", "Add Assignees String") do |v|
     options[:assignees] = true
   end
 
-  opts.on("--sveltegrep", "Remove Extracted Script Extension") do |v|
+  opts.on("--sveltegrep", "Remove Extracted Script Extension, and use semgrep blocklist") do |v|
     options[:sveltegrep] = true
+    options[:matcher] = Matcher.new(DEFAULT_MATCHER_FILENAME, "#{ENV["SCRIPTPATH"]}/semgrep_rules/blocklist.txt")
+  end
+
+  opts.on("--semgrep", "Use semgrep blocklist") do |v|
+    options[:semgrep] = true
+    options[:matcher] = Matcher.new(DEFAULT_MATCHER_FILENAME, "#{ENV["SCRIPTPATH"]}/semgrep_rules/blocklist.txt")
   end
 end.parse!
 
@@ -39,5 +79,5 @@ STDIN.each_line(chomp: true).sort.uniq.to_a.each do |l|
     l.gsub!(/$/, "<br>Cc #{ENV['ASSIGNEES']}")
   end
 
-  puts l
+  puts l unless options[:matcher].match?(l)
 end
