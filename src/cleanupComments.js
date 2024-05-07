6cf336ad50ac36f433a06f03967f2b6d5ed61147
Andrea Brancaleoni <abc@pompel.me>
2024-05-07 18:09:30 +0200

    *: fix formatting

--- src/cleanupComments.js
+++ src/cleanupComments.js
@@ -1,15 +1,15 @@
-export default async function cleanupComments({
-    github,
-    githubToken,
-    context
+export default async function cleanupComments ({
+  github,
+  githubToken,
+  context
 }) {
-    if (!github && githubToken) {
-        const { Octokit } = await import("octokit");
+  if (!github && githubToken) {
+    const { Octokit } = await import('octokit')
 
-        github = new Octokit({ auth: githubToken })
-    }
-    
-    var query = `query($owner:String!, $name:String!, $prnumber:Int!) { 
+    github = new Octokit({ auth: githubToken })
+  }
+
+  const query = `query($owner:String!, $name:String!, $prnumber:Int!) { 
         repository(owner:$owner, name:$name) { 
           pullRequest(number:$prnumber) {
             reviewThreads(last:100) {
@@ -29,36 +29,36 @@ export default async function cleanupComments({
             }
           }
         }
-      }`;
-      const variables = {
-        owner: context.repo.owner,
-        name: context.repo.repo,
-        prnumber: context.issue.number
-      }
-      const result = await github.graphql(query, variables);
-      const threads = result.repository.pullRequest.reviewThreads;
-      const deletableComments = threads.nodes.filter(
-        reviewThread => (
-          reviewThread.isOutdated === true &&
+      }`
+  const variables = {
+    owner: context.repo.owner,
+    name: context.repo.repo,
+    prnumber: context.issue.number
+  }
+  const result = await github.graphql(query, variables)
+  const threads = result.repository.pullRequest.reviewThreads
+  const deletableComments = threads.nodes.filter(
+    reviewThread => (
+      reviewThread.isOutdated === true &&
           reviewThread.comments.totalCount === 1 &&
-          reviewThread.comments.nodes[0].author.login === "github-actions" &&
-          reviewThread.comments.nodes[0].body.includes("<br>Cc ")
-        )
-      ).map(
-        reviewThread => (
-          reviewThread.comments.nodes[0].id
-        )
-      );
-      console.log("Delete", deletableComments);
-      if (deletableComments) {
-        deleteMutation = `mutation($comment:ID!) {
+          reviewThread.comments.nodes[0].author.login === 'github-actions' &&
+          reviewThread.comments.nodes[0].body.includes('<br>Cc ')
+    )
+  ).map(
+    reviewThread => (
+      reviewThread.comments.nodes[0].id
+    )
+  )
+  console.log('Delete', deletableComments)
+  if (deletableComments) {
+    const deleteMutation = `mutation($comment:ID!) {
           deletePullRequestReviewComment(input: {id:$comment}) {
             clientMutationId
           }
-        }`;
-        for (const commentId of deletableComments) {
-          console.log("Deleting %s", commentId);
-          await github.graphql(deleteMutation, {comment: commentId});
-        }
-      }
+        }`
+    for (const commentId of deletableComments) {
+      console.log('Deleting %s', commentId)
+      await github.graphql(deleteMutation, { comment: commentId })
+    }
+  }
 }
--- src/dependabotDismiss.js
+++ src/dependabotDismiss.js
@@ -1,106 +1,106 @@
-import fs from 'node:fs/promises';
+import fs from 'node:fs/promises'
 
 const Severity = {
-    low: 0,
-    medium: 1,
-    high: 2,
-    critical: 3,
+  low: 0,
+  medium: 1,
+  high: 2,
+  critical: 3
 }
 
-export default async function dependabotDismiss({
-    org,
-    minlevel = Severity.low,
-    debug = false,
-    hotwords = [' dos ',
-        'denial of service',
-        'redos',
-        'denial-of-service',
-        'memory explosion',
-        'inefficient regular expression',
-        'regular expression complexity'],
-    githubToken = null,
-    github = null,
-    actor = 'security-action',
-    dependabotDismissConfig = 'dependabot-dismiss.txt',
+export default async function dependabotDismiss ({
+  org,
+  minlevel = Severity.low,
+  debug = false,
+  hotwords = [' dos ',
+    'denial of service',
+    'redos',
+    'denial-of-service',
+    'memory explosion',
+    'inefficient regular expression',
+    'regular expression complexity'],
+  githubToken = null,
+  github = null,
+  actor = 'security-action',
+  dependabotDismissConfig = 'dependabot-dismiss.txt'
 }) {
-    let watermark = "The following alerts were dismissed:\n\n";
-    let message = '';
+  const watermark = 'The following alerts were dismissed:\n\n'
+  let message = ''
 
-    let dependabotDismissIds = [];
-    
-    try {
-        dependabotDismissIds = (await fs.readFile(dependabotDismissConfig, 'utf-8')).split('\n').map(l => l.trim()).filter(Boolean);
-    } catch (e) {
-        if (debug) console.log(`Could not read ${dependabotDismissConfig}: ${e}`);
-    }
+  let dependabotDismissIds = []
 
-    if (!github && githubToken) {
-        const { Octokit } = await import("octokit");
+  try {
+    dependabotDismissIds = (await fs.readFile(dependabotDismissConfig, 'utf-8')).split('\n').map(l => l.trim()).filter(Boolean)
+  } catch (e) {
+    if (debug) console.log(`Could not read ${dependabotDismissConfig}: ${e}`)
+  }
 
-        github = new Octokit({ auth: githubToken })
-    }
+  if (!github && githubToken) {
+    const { Octokit } = await import('octokit')
 
-    if (!github && !githubToken) {
-        throw new Error('either githubToken or github is required!');
-    }
+    github = new Octokit({ auth: githubToken })
+  }
 
-    debug = debug === 'true' || debug === true;
-    
-    if (typeof severity === 'string') {
-        severity = Severity[severity];
-    }
+  if (!github && !githubToken) {
+    throw new Error('either githubToken or github is required!')
+  }
 
-    const alerts = Array.from(await github.paginate('GET /orgs/{org}/dependabot/alerts', {
-        org: org,
-        headers: {
-          'X-GitHub-Api-Version': '2022-11-28'
-        },
-        sort: 'updated',
-        state: 'open',
-        severity: Object.keys(Severity).filter(s => Severity[s] >= minlevel)
-    })).filter(a => 
-        hotwords.some(h => a.security_advisory.summary.toLowerCase().includes(h)) ||
+  debug = debug === 'true' || debug === true
+
+  if (typeof minlevel === 'string') {
+    minlevel = Severity[minlevel]
+  }
+
+  const alerts = Array.from(await github.paginate('GET /orgs/{org}/dependabot/alerts', {
+    org,
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28'
+    },
+    sort: 'updated',
+    state: 'open',
+    severity: Object.keys(Severity).filter(s => Severity[s] >= minlevel)
+  })).filter(a =>
+    hotwords.some(h => a.security_advisory.summary.toLowerCase().includes(h)) ||
             dependabotDismissIds.includes(a.security_advisory.ghsa_id) ||
             dependabotDismissIds.includes(a.security_advisory.cve_id)
-    );
+  )
 
-    for (const a of alerts) {
-        // get the first hotword that matches the summary
-        const hotword = hotwords.find(h => a.security_advisory.summary.toLowerCase().includes(h));
-        const matchId = dependabotDismissIds.find(id => a.security_advisory.ghsa_id === id || a.security_advisory.cve_id === id);
-        let dismissComment = `Dismissed by ${actor}`;
-        if (matchId) {
-            dismissComment += ` because the alert matched the id "${matchId}"`;
-        } else {
-            dismissComment += ` because the alert summary contains the hotword "${hotword}"`;
-        }
-        
-        message += `- [${a.security_advisory.summary} in \`${org}/${a.repository.name}\`](${a.html_url})\n`
-
-        if (debug) {
-            console.log(dismissComment);
-            console.log(`Summary: ${a.security_advisory.summary}`);
-            console.log(`GHSA: ${a.security_advisory.ghsa_id}`);
-            console.log(`CVE: ${a.security_advisory.cve_id}`);
-            continue;
-        }
-
-        await github.request('PATCH /repos/{org}/{repo}/dependabot/alerts/{alert_number}', {
-            org: org,
-            repo: a.repository.name,
-            alert_number: a.number,
-            dismissed_reason: 'tolerable_risk',
-            dismissed_comment: dismissComment,
-            headers: {
-                'X-GitHub-Api-Version': '2022-11-28'
-            },
-            state: 'dismissed',
-        });
-    }
-
-    if (message.length > 0) {
-        return watermark + message;
+  for (const a of alerts) {
+    // get the first hotword that matches the summary
+    const hotword = hotwords.find(h => a.security_advisory.summary.toLowerCase().includes(h))
+    const matchId = dependabotDismissIds.find(id => a.security_advisory.ghsa_id === id || a.security_advisory.cve_id === id)
+    let dismissComment = `Dismissed by ${actor}`
+    if (matchId) {
+      dismissComment += ` because the alert matched the id "${matchId}"`
     } else {
-        return '';
+      dismissComment += ` because the alert summary contains the hotword "${hotword}"`
     }
+
+    message += `- [${a.security_advisory.summary} in \`${org}/${a.repository.name}\`](${a.html_url})\n`
+
+    if (debug) {
+      console.log(dismissComment)
+      console.log(`Summary: ${a.security_advisory.summary}`)
+      console.log(`GHSA: ${a.security_advisory.ghsa_id}`)
+      console.log(`CVE: ${a.security_advisory.cve_id}`)
+      continue
+    }
+
+    await github.request('PATCH /repos/{org}/{repo}/dependabot/alerts/{alert_number}', {
+      org,
+      repo: a.repository.name,
+      alert_number: a.number,
+      dismissed_reason: 'tolerable_risk',
+      dismissed_comment: dismissComment,
+      headers: {
+        'X-GitHub-Api-Version': '2022-11-28'
+      },
+      state: 'dismissed'
+    })
+  }
+
+  if (message.length > 0) {
+    return watermark + message
+  } else {
+    return ''
+  }
 }
