3bf7ddebd8725d21f55a78f0a96e2fdcbb1d02ab
Andrea Brancaleoni <abc@pompel.me>
2024-05-07 15:53:25 +0200

    action.yml: assignee removed

---
+++ src/assigneeRemoved.js
@@ -0,0 +1,43 @@
+export default async function assigneeRemoved ({
+  context,
+  github,
+  githubToken
+}) {
+  const { ASSIGNEES } = process.env
+  console.log('assignees: %o', ASSIGNEES)
+  const assignees = ASSIGNEES.split(/\s+/).filter((str) => str !== '')
+  const query = `query ($owner: String!, $name: String!, $prnumber: Int!) {
+      repository(owner: $owner, name: $name) {
+        pullRequest(number: $prnumber) {
+          timelineItems(last: 100, itemTypes: UNLABELED_EVENT) {
+            nodes {
+              ... on UnlabeledEvent {
+                label {
+                  name
+                }
+                actor {
+                  login
+                }
+              }
+            }
+          }
+        }
+      }
+    }`
+  const variables = {
+    owner: context.repo.owner,
+    name: context.repo.repo,
+    prnumber: context.issue.number
+  }
+  const result = await github.graphql(query, variables)
+  const timelineItems = result.repository.pullRequest.timelineItems
+  console.log('timelineItems: %o', timelineItems)
+  const removedByAssigneeEvents = timelineItems.nodes.filter(
+    timelineItem => (
+      timelineItem.label.name === 'needs-security-review' &&
+        assignees.some((a) => timelineItem.actor.login === a)
+    )
+  ).length
+  console.log('RemovedByAssigneeEvents: %d', removedByAssigneeEvents)
+  return removedByAssigneeEvents > 0
+}
