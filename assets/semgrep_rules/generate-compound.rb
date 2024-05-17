620448c778911cd0f7584cccfb17d3dc8f86fec8
Andrea Brancaleoni <abc@pompel.me>
2024-05-17 17:08:31 +0200

    generate-compound.rb: generate compound.yml file for cmd line tests

---
+++ assets/semgrep_rules/generate-compound.rb
@@ -0,0 +1,28 @@
+require 'yaml'
+
+SEMGREP_VERSION = `semgrep --version`.strip
+
+HOST = 'https://semgrep.dev'
+
+files = Dir['client/*.yaml', 'services/*.yaml', 'generated/*/vulns.yaml', 'generated/*/audit.yaml']
+
+rules = {'rules' => []}
+
+files.each do |fname|
+	begin
+		irules = YAML.load(File.read(fname))['rules']
+		puts "#{fname}: #{irules.length}"
+
+		rules['rules'].concat irules
+	rescue
+		puts "Error in #{fname}"
+	end
+end
+
+puts "#rules: #{rules['rules'].length}"
+
+File.write("compound.yaml", YAML.dump(rules))
+
+# require 'pry'
+# binding.pry
+
