d739070b26bdfb63686e5652d0427944f0c48e0e
Artem Chaikin <chaykin.artem@gmail.com>
2024-10-18 16:42:21 +0100

    add size_t to reinterpret_cast rule

--- assets/semgrep_rules/client/reinterpret_cast.cpp
+++ assets/semgrep_rules/client/reinterpret_cast.cpp
@@ -10,3 +10,5 @@ int rv = PKCS5_PBKDF2_HMAC(mnemonic.data(), mnemonic.length(), reinterpret_cast<
 float* float_data = reinterpret_cast<float*>(const_cast<uint8_t*>(data));
 // ok: reinterpret_cast
 auto orig_fn = reinterpret_cast<GetModuleFileNameExWFunction>(g_originals.functions[GET_MODULE_FILENAME_EX_W_ID]);
+// ruleid: reinterpret_cast
+size_t bytes_read = reinterpret_cast<size_t>(arg0);
