e520cb2246afe118929c77f46c3eb71a63b0ae1d
Artem Chaikin <chaykin.artem@gmail.com>
2025-04-15 14:19:57 +0100

    remove unsafe_buffers and unsafe_todo

--- assets/semgrep_rules/client/unsafe-cpp-constructs.cpp
+++ assets/semgrep_rules/client/unsafe-cpp-constructs.cpp
@@ -5,10 +5,6 @@
 #pragma allow_unsafe_buffers
 #endif
 // ruleid: unsafe_cpp_constructs
-UNSAFE_BUFFERS(data());
-// ruleid: unsafe_cpp_constructs
-UNSAFE_TODO(base::make_span(&web_script_source, 1u));
-// ruleid: unsafe_cpp_constructs
 std::next(it);
 // ruleid: unsafe_cpp_constructs
 std::advance(cert_iter, cert_idx);
