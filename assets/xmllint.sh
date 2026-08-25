d8170ce528ad0105d88e61b29c00813e480a24f2
Jakub Wądołowski <6334715+jwadolowski@users.noreply.github.com>
2026-08-25 11:00:17 +0200

    fix(shell): Introduce pipefail to xmllint.sh

--- assets/xmllint.sh
+++ assets/xmllint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
+set -o pipefail
 # Absolute path to this script. /home/user/bin/foo.sh
 SCRIPT=$(readlink -f "$0")
 # Absolute path this script is in. /home/user/bin
@@ -8,8 +9,4 @@ export SCRIPTPATH
 # Only check SVGs
 [[ "$1" == *".svg" ]] || exit 0
 
-OUT=$(xmllint --dtdvalid "$SCRIPTPATH/dtd/svg11-secure-flat.dtd" --noout "$1" 2>&1) || {
-    echo "$OUT"
-    exit 1
-}
-echo "$OUT" | grep -v '^Document'
+xmllint --dtdvalid "$SCRIPTPATH/dtd/svg11-secure-flat.dtd" --noout "$1" 2>&1 | { grep -v '^Document' || true; }
