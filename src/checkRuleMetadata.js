3c70f699a362914a5026177aede476d61167be04
Andrea Brancaleoni <abc@pompel.me>
2026-09-21 20:52:15 -0700

    fix: import js-yaml namespace for v5 ESM compat
    js-yaml v5 is pure ESM with named exports only; a default
    import fails to load the module and breaks lint-rules.

--- src/checkRuleMetadata.js
+++ src/checkRuleMetadata.js
@@ -1,6 +1,6 @@
 import fs from 'fs'
 import path from 'path'
-import yaml from 'js-yaml'
+import * as yaml from 'js-yaml'
 
 function checkRuleFile (filePath, basePath = process.cwd()) {
   const errors = []
